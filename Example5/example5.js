7d393bb097b5785cd603528874eaa6177f1b4189
pradeep <pradeep.misra@gmail.com>
2021-12-17 20:53:10 +0530

    apply html tags using jQuery

---
+++ Example5/example5.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("h1").text('Welcome');
+    $("button").text("Please don't click me!")
+
+});
+
---
+++ Example6/example6.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("h1").text('Welcome');
+    $("button").html("<em>Click</em>")
+
+});
+
