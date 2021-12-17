ecd667c79fbc14425f410ed813fc4e420fc91ff3
pradeep <pradeep.misra@gmail.com>
2021-12-17 20:37:40 +0530

    added jQuery examples

--- Example2/example2.js
+++ Example2/example2.js
@@ -1,1 +1,7 @@
-$("h1").css('color', 'red');
+$(document).ready(function() {
+    $("h1").css('color', 'green');
+    console.log($("h1").css('color'));
+    console.log($("h1").css('font-size'));
+    console.log($("h1").css('font-size', '5rem'));
+});
+
