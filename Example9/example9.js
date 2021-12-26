fdbc47896a1359a3e49e458a298e8827e3440f0f
pradeep <pradeep.misra@gmail.com>
2021-12-26 19:36:31 +0530

    Exp9: Keypress event listener for input box

--- Example9/example9.js
+++ Example9/example9.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
     $(document).keypress(function(event){
         $("h1").text(event.key);
     })
+
+    $("input").keypress(function(event) {
+        console.log(event.key);
+    })
 });
 
